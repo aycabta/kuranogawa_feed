72e89bd4e9f9af74d7740a4024c01d2c6d174ef3
Code Ass <aycabta@gmail.com>
2015-10-18 19:19:24 +0900

    Define variable

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ request({
     body = ic.convert(body).toString();
     var $ = cheerio.load(body);
     var author = 'タウンマネージャー 久領堤 纒';
-    feed = new RSS({
+    var feed = new RSS({
         title: $('title').text(),
         feed_url: 'http://kuranogawa-feed.chijin-dokku.tk',
         site_url: 'http://p.eagate.573.jp/game/bemani/hinabita/p/kuranogawa/',
